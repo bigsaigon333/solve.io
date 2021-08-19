a4ec707b18fddf0b89f706b1e0058797397c75a3
bigsaigon333 <likepeppermint@gmail.com>
2021-08-19 23:26:23 +0900

    build: 0.0.2버젼을 배포한다

--- dist/index.js
+++ dist/index.js
@@ -42,6 +42,23 @@ var path_1 = __importDefault(require("path"));
 var util_1 = __importDefault(require("util"));
 var child_process_1 = __importDefault(require("child_process"));
 var exec = util_1.default.promisify(child_process_1.default.exec);
+/**
+ * @params filePath - PS코드의 절대경로
+ * @returns solve - 입력값을 인자로 받아 PS 코드 실행 결과를 string으로 반환하는 함수
+ *
+ * @example // BoJ 1000번 문제 풀이 1000.test.js
+  const path = require("path");
+  const genSolve = require("solve.io");
+  const solve = genSolve(path.resolve(__dirname, "1000.js"));
+
+  test("예제 1", async () => {
+    const input = `1 2`;
+    const output = `3
+  `;
+
+    expect(await solve(input)).toBe(output);
+  });
+ */
 var generateSolve = function (filePath) {
     if (!path_1.default.isAbsolute(filePath)) {
         throw new TypeError("Invalid filePath: " + filePath + "\nfilePath should be an absolute path");
